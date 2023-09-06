976cf71e98304a1f3f72ada284c6ee0fca4a7b52
Fabien Fiette <fabienfiette@hotmail.fr>
2023-09-06 08:56:37 +0200

    feat: posts route, create, get and delete endpoint

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 import express from "express";
 import cors from "cors";
+import postsCRD from "./routes/posts.js";
 
 const app = express()
 const port = process.env.SERVER_PORT || 3001
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send("Server express status: ON")
 })
+
+app.use("/posts", postsCRD)
 
 app.get('*', (req, res) => {
     res.status(404).send('Not found');
