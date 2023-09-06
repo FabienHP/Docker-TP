731639e32cc5b393b467f318839e082d5ac16027
Fabien Fiette <fabienfiette@hotmail.fr>
2023-09-06 14:36:49 +0200

    feat: EXPRESS LOG CONNECTED TO LOGSTASH

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // Get a list of posts
 router.get("/", async (req, res) => {
-    logger.debug("GET /posts" + req.origins)
+    logger.debug("GET /posts with origins: " + req.origins)
     let results = await PostsModel.find({});
 
     res.status(200).send(results);
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
 
 // Get a single post
 router.get("/:id", async (req, res) => {
-    logger.debug("GET /posts/:id" + req.origins)
+    logger.debug("GET /posts/:id with origins: " + req.origins)
     let query = {_id: req.params.id};
     let result = await PostsModel.findOne(query);
 
@@ -23,14 +23,14 @@ router.get("/:id", async (req, res) => {
 
 // Add a new document to the collection
 router.post("/", async (req, res) => {
-    logger.debug("POST /posts" + req.origins)
+    logger.debug("POST /posts with origins: " + req.origins)
     let result = await PostsModel.create(req.body);
     res.status(204).send(result);
 });
 
 // Delete an entry
 router.delete("/:id", async (req, res) => {
-    logger.debug("DELETE /posts/:id" + req.origins)
+    logger.debug("DELETE /posts/:id with origins: " + req.origins)
     const query = {_id: req.params.id};
 
     let result = await PostsModel.findOneAndDelete(query);
